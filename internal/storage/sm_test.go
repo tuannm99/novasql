4246a8ff22a858d3a3e0b492efb59830e581d177
tuannm99 <gm.tuannm99@gmail.com>
2025-09-03 23:19:07 +0700

    add: test

--- internal/storage/sm_test.go
+++ internal/storage/sm_test.go
@@ -1,34 +1,19 @@
 package storage_test
-//
-// import (
-// 	"testing"
-//
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/tuannm99/novasql/internal/storage/common"
-// )
-//
-// func TestManager(t *testing.T) {
-// 	// tạo SM + FileSet cho quan hệ "12345" trong thư mục "data/base/16384"
-// 	sm := NewStorageManager(common.Embedded, "data")
-// 	fs := LocalFileSet{Dir: "data/base/16384", Base: "12345"}
-//
-// 	// Load page
-// 	pg, err := sm.LoadPage(fs, 0)
-// 	if err != nil { /* ... */
-// 	}
-// 	if pg.IsUninitialized() {
-// 		pg.init(0)
-// 	}
-//
-// 	// Insert
-// 	slot, ok := pg.InsertTuple([]byte("hello=world"))
-// 	if !ok {
-// 		// chuyển page khác
-// 	}
-//
-// 	// Read
-// 	data, ok := pg.ReadTuple(slot)
-//
-// 	// Save
-// 	_ = sm.SavePage(fs, 0, pg)
-// }
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tuannm99/novasql/internal/storage"
+)
+
+func TestStorageManager(t *testing.T) {
+	// tạo SM + FileSet cho quan hệ "12345" trong thư mục "data/base/16384"
+	sm := storage.NewStorageManager("data")
+	fs := storage.LocalFileSet{Dir: "data/base/16384", Base: "12345"}
+
+	// Load page
+	pg, err := sm.LoadPage(fs, 0)
+	assert.NotNil(t, err)
+	assert.NotNil(t, pg)
+}
